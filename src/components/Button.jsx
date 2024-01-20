e1910f2cf35f6b9e1af8053f193aa868fcca682b
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-01-20 16:52:12 +0700

    UPDATE managing state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,22 @@
 // eslint-disable-next-line react/prop-types
-export default function Button({ children = "button", action = "default" }) {
+export default function Button({ children = "button", action = "default", actionOnClick }) {
   if (action === "default") {    
     return (
-      <button className="mt-10 rounded px-8 py-2 bg-cyan-100 text-cyan-600 font-semibold">
+      <button onClick={actionOnClick} className="mt-10 rounded px-8 py-2 bg-cyan-100 text-cyan-600 font-semibold">
         {children}
       </button>
     )
   }
   else if (action === "update") {
     return (
-      <button className="mt-10 rounded px-8 py-2 bg-yellow-100 text-yellow-600 font-semibold">
+      <button onClick={actionOnClick} className="mt-10 rounded px-8 py-2 bg-yellow-100 text-yellow-600 font-semibold">
         {children}
       </button>
     )
   }
   else if (action === "delete") {
     return (
-      <button className="mt-10 rounded px-8 py-2 bg-red-100 text-red-600 font-semibold">
+      <button onClick={actionOnClick} className="mt-10 rounded px-8 py-2 bg-red-100 text-red-600 font-semibold">
         {children}
       </button>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,37 +3,30 @@ import Button from '../components/Button'
 import Card from '../components/Card'
 
 export default function Home() {
-  const [stateSatu] = useState([
+
+  const [valueInputBaru, setValueInputBaru] = useState("Mahasiswa")
+  const [mahasiswa, setMahasiswa] = useState([
     {
       nama: "sendhyrama",
-      umur: 22
+      umur: 22,
+      deskripsi: "sidjiadjsaodjosaidoiiowqiodjioawjdioajdoia"
     },
     {
       nama: "hilman",
-      umur: 21
+      umur: 21,
+      deskripsi: "sidjiadjsaodjosaidoiiowqiodjioawjdioajdoia"
     },
     {
       nama: "rendi",
-      umur: 21
+      umur: 21,
+      deskripsi: "sidjiadjsaodjosaidoiiowqiodjioawjdioajdoia"
     },
     {
       nama: "jose",
-      umur: 21
+      umur: 21,
+      deskripsi: "sidjiadjsaodjosaidoiiowqiodjioawjdioajdoia"
     },
   ])
-
-  // function ubahState(e) {
-  //   e.preventDefault()
-
-  //   setStateSatu(stateSatu + 1)
-  // }
-
-  // function ubahVariabelBiasa(e) {
-  //   e.preventDefault()
-
-  //   variabelBiasa = variabelBiasa + 1
-  //   console.log(variabelBiasa)
-  // }
 
   return (
     <main className="w-full min-h-screen bg-gray-50 flex justify-center items-center p-20">
@@ -42,17 +35,40 @@ export default function Home() {
           <h1 className="font-bold text-5xl text-pink-600">Halo dunia</h1>
         </header>
         <body>
-          <div>
-            <Card judul="Ini card pertama" >
-              Props adalah singkatan dari properti , props itu punya fungsi sebagai data yang dapat dikirimkan ke dalam sebuah komponen sehingga komponen tersebut dapat menggunakan data yang dikirmkan.
-            </Card>
-
-            <Card judul="bldsadlapd">
-              Props adalah singkatan dari properti , props itu punya fungsi sebagai data yang dapat dikirimkan ke dalam sebuah komponen sehingga komponen tersebut dapat menggunakan data yang dikirmkan.</Card>
-            <Button action='delete'>
-              Klik disini
-            </Button>
+          <div className="flex gap-2 flex-wrap">
+            {mahasiswa.map((value,index) => {
+              return (
+                <Card judul={value.nama} key={index} buttonTitle={value.umur}>
+                  {value.deskripsi}
+                </Card>
+              )
+            })}
           </div>
+          <input type="text" value={valueInputBaru} onChange={(e)=>{setValueInputBaru(e.target.value)}} />
+          <Button actionOnClick={() => {
+            setMahasiswa([...mahasiswa, {nama: valueInputBaru}])
+          }}>
+            Tambah list mahasiswa
+          </Button>
+
+          <Button action="delete" actionOnClick={() => {
+            setMahasiswa(mahasiswa.filter((value) => {
+              return value.nama !== "hilman" 
+            }))
+          }}>
+            Hapus list mahasiswa
+          </Button>
+
+          <Button action="update" actionOnClick={() => {
+            setMahasiswa(mahasiswa.map((value) => {
+              if (value.nama === "hilman") {
+                return {nama: value.nama, umur: value.umur, deskripsi: "Hilman adalah anak informatika"}
+              }
+              return value
+            }))
+          }}>
+            Update
+          </Button>
         </body>
       </di>
     </main>
